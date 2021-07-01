65daa1a6a18920058c4c092dbc93801917d7d294
Harshit Srivastava <41013322+TechAllByHarshit@users.noreply.github.com>
2021-07-01 19:27:15 +0530

    Delete lib directory

--- lib/ping/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { Message, Client } from "discord.js";
-export interface details {
-    message: Message;
-    client: Client;
-}
-export declare function ping(props: details): Promise<void>;
